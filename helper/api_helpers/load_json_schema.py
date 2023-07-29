378cb1c9f6a24dc14fe977a0d82a041e95ae8d2b
marcelinleen <94484224+marcelinleen@users.noreply.github.com>
2023-07-30 02:28:02 +0600

    changes for tests

--- helper/api_helpers/load_json_schema.py
+++ helper/api_helpers/load_json_schema.py
@@ -3,6 +3,6 @@ import json
 
 
 def load_json_schema(filename: str):
-    schema_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'schemas', filename)
+    schema_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'schemas', filename)
     with open(schema_path) as schema:
         return json.loads(schema.read())
