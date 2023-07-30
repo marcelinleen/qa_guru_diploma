aa019f86a3668acd68ceedde488b2f5ced3fdf76
marcelinleen <94484224+marcelinleen@users.noreply.github.com>
2023-07-30 12:40:54 +0600

    tests changes

--- helper/api_helpers/check_following_list.py
+++ helper/api_helpers/check_following_list.py
@@ -1,22 +1,24 @@
 from pages.web_pages.user_page import UserPage
 from helper.api_helpers.api_get_user_following import get_user_following_list
+from selene import be
 
 
 def clean_following_list(browser, username, another_username, api_key):
 
+    user_page = UserPage(browser)
+    user_page.open(another_username)
+
     following_list = get_user_following_list(username, api_key)
 
-    if another_username in following_list:
-        user_page = UserPage(browser)
-        user_page.open(another_username)
+    if (another_username in following_list) or (browser.element('[data-analytics-action="UnfollowUser"]').is_displayed()):
         user_page.unfollow_user()
 
 
 def add_following(browser, username, another_username, api_key):
+    user_page = UserPage(browser)
+    user_page.open(another_username)
 
     following_list = get_user_following_list(username, api_key)
 
-    if another_username not in following_list:
-        user_page = UserPage(browser)
-        user_page.open(another_username)
+    if (another_username not in following_list) or browser.element('[data-analytics-action="FollowUser"]').is_displayed():
         user_page.follow_user()
--- pages/web_pages/user_page.py
+++ pages/web_pages/user_page.py
@@ -9,8 +9,6 @@ class UserPage:
 
     def follow_user(self):
         self.browser.element('[data-analytics-action="FollowUser"]').click()
-        self.browser.element('[data-analytics-action="UnfollowUser"]').wait_until(be.visible)
 
     def unfollow_user(self):
         self.browser.element('[data-analytics-action="UnfollowUser"]').click()
-        self.browser.element('[data-analytics-action="FollowUser"]').wait_until(be.visible)
